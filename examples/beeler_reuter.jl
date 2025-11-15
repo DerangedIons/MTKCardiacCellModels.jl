ae60d4e142eb24455b1bb9c960b00f1f89a9fef0
termi-official <9196588+termi-official@users.noreply.github.com>
2025-11-15 02:59:43 +0100

    Major refactor

--- examples/beeler_reuter.jl
+++ examples/beeler_reuter.jl
@@ -1,67 +1,60 @@
-using RushLarsenSolvers
+# using RushLarsenSolvers
 using MTKCardiacCellModels
-using MTKCardiacCellModels: AlphaBetaGate, IonChannelConductance, IonCurrent, LeakCurrent,
-    SlowInwardCurrent, TimeIndependentK, TimeActivatedOutward, CalciumDynamics, StimulationSystem
+using MTKCardiacCellModels: AlphaBetaGate, NernstPotential, OhmicCurrent
+using MTKCardiacCellModels: BeelerReuterIK1, BeelerReuterIx1, BeelerReuterICa, BeelerReuterINa, BeelerReuterCalciumDynamics
 using ModelingToolkit
-using Plots
+# using Plots
 
 import ModelingToolkit: t_nounits as t, D_nounits as D
 
-@variables V(t) Ca_i(t)
-V = GlobalScope(V)
-Ca_i = GlobalScope(Ca_i)
-
-# Fast sodium current (INa) gates
-@named m = AlphaBetaGate(
-    α_expr=(V + 47.0) / (1.0 - exp(-0.1 * (V + 47.0))),
-    β_expr=40.0 * exp(-0.056 * (V + 72.0))
-)
-
-@named h = AlphaBetaGate(
-    α_expr=0.126 * exp(-0.25 * (V + 77.0)),
-    β_expr=1.7 / (1.0 + exp(-0.082 * (V + 22.5)))
-)
-
-@named j = AlphaBetaGate(
-    α_expr=0.055 * exp(-0.25 * (V + 78)) / (1 + exp(-0.2 * (V + 78))),
-    β_expr=0.3 / (1 + exp(-0.1 * (V + 32.0)))
-)
-
-# Fast sodium channel conductance (includes leak component)
-@named sodium = IonChannelConductance(g_max=4.0, gates=[m, h, j], powers=[3.0, 1.0, 1.0])
-@named sod_current = IonCurrent(Vm=V, gm=sodium, Ex=50.0)
-
-# Sodium leak current
-@named na_leak = LeakCurrent(Vm=V, gL=0.003, EL=50.0)
-
-# Slow inward current (Is) gates
-@named d = AlphaBetaGate(
-    α_expr=0.095 * exp(-0.01 * (V - 5.0)) / (1.0 + exp(-0.072 * (V - 5.0))),
-    β_expr=0.07 * exp(-0.017 * (V + 44.0)) / (1.0 + exp(0.05 * (V + 44.0)))
-)
-
-@named f = AlphaBetaGate(
-    α_expr=0.012 * exp(-0.008 * (V + 28.0)) / (1.0 + exp(0.15 * (V + 28.0))),
-    β_expr=0.0065 * exp(-0.02 * (V + 30.0)) / (1.0 + exp(-0.2 * (V + 30.0)))
-)
-
-# Slow inward current (calcium)
-@named slow_inward = IonChannelConductance(g_max=0.09, gates=[d, f])
-
-@named slow_current = SlowInwardCurrent(Vm=V, Ca_i=Ca_i, gm=slow_inward)
-
-
-@named k1_current = TimeIndependentK(Vm=V)
-
-# Time-activated outward current (Ix1) gate
-@named x1 = AlphaBetaGate(
-    α_expr=0.0005 * exp(0.083 * (V + 50.0)) / (1.0 + exp(0.057 * (V + 50.0))),
-    β_expr=0.0013 * exp(-0.06 * (V + 20.0)) / (1.0 + exp(-0.04 * (V + 20.0)))
-)
-
-@named x1_current = TimeActivatedOutward(Vm=V, gate=x1, g_x1=0.8)
-
-@named calcium_dyn = CalciumDynamics(Ca_i=Ca_i, I_s=slow_current.Im)
+@mtkmodel BeelerReuterModel begin
+    @structural_parameters begin
+        I_stim
+    end
+    @parameters begin
+        gNaL = 0.01
+        ENa = 50.0
+        Cₘ = 1.0
+    end
+    @variables begin
+        φₘ(t)
+        Caᵢ(t)
+    end
+    @components begin
+        # Potentials
+        ECa = NernstPotential(zₓ=2)
+        # Channels
+        cNa_leak = OhmicCurrent(g = gNaL)
+        cK1 = BeelerReuterIK1()
+        cx1 = BeelerReuterIx1()
+        cCa = BeelerReuterICa()
+        cNa = BeelerReuterINa()
+        # Concentrations
+        calcium = BeelerReuterCalciumDynamics()
+    end
+    @equations begin
+        # Reversal Potential
+        ECa.Xᵢ ~ Caᵢ
+        ECa.Xₑ ~ Caᵢ
+        # Membrane equation
+        D(φₘ) ~ -1 / Cₘ * (cNa_leak.Iₘ + cNa.Iₘ + cK1.Iₘ + cx1.Iₘ + cCa.Iₘ - I_stim(t))
+        # Leak current
+        cNa_leak.φₘ ~ φₘ
+        cNa_leak.E  ~ ENa
+        # x1 channel
+        cx1.φₘ ~ φₘ
+        # K1 channel
+        cK1.φₘ ~ φₘ
+        # INa channel
+        cNa.φₘ ~ φₘ
+        # Slow inward current
+        cCa.φₘ ~ φₘ
+        cCa.ECa ~ ECa.Eₓ
+        # Calcium dynamics
+        calcium.Caᵢ ~ Caᵢ
+        calcium.iCa ~ cCa.Iₘ
+    end
+end
 
 function rounded_pulse(t_c; t_start=20.0, duration=5.0, amplitude=10.0, smoothness=0.1)
     """
@@ -87,57 +80,39 @@ end
 
 @register_symbolic rounded_pulse(t)
 
-
-@named stim = StimulationSystem(stim_func=rounded_pulse)
-
-
-@component function BeelerReuterCompartment(; name, Vm, Cm, currents, I_stim, Ca_dyn)
-    @parameters Cm = Cm
-    Is = [current.Im for current in currents]
-
-    eqs = [D(Vm) ~ -1 / Cm * (sum(Is) - I_stim)]
-
-    System(eqs, t, [Vm, I_stim], [Cm], systems=[currents..., Ca_dyn], name=name)
-end
-
-@named com = BeelerReuterCompartment(
-    Vm=V,
-    Cm=1.0,
-    currents=[sod_current, na_leak, slow_current, k1_current, x1_current],
-    I_stim=stim.I_stim,
-    Ca_dyn=calcium_dyn
-)
-
-
-@named beeler_reuter_system = compose(com, stim)
-
-sys = structural_simplify(beeler_reuter_system)
-
-rlf = RushLarsenFunction(sys)
+@mtkcompile beeler_reuter_system = BeelerReuterModel(;I_stim=rounded_pulse)
 
 u0 = [
-    1e-7,       # Ca_i(t) - Intracellular calcium concentration [mM]
-    0.0001,     # x1_current₊x1₊y(t) - Outward current activation gate
-    0.994,      # slow_current₊slow_inward₊f₊y(t) - Calcium inactivation gate
-    0.003,      # slow_current₊slow_inward₊d₊y(t) - Calcium activation gate
+    1e-7,       # Caᵢ(t) - Intracellular calcium concentration [mM]
     0.975,      # sod_current₊sodium₊j₊y(t) - Sodium slow inactivation gate
     0.988,      # sod_current₊sodium₊h₊y(t) - Sodium fast inactivation gate
     0.011,      # sod_current₊sodium₊m₊y(t) - Sodium activation gate
+    0.994,      # slow_current₊slow_inward₊f₊y(t) - Calcium inactivation gate
+    0.003,      # slow_current₊slow_inward₊d₊y(t) - Calcium activation gate
+    0.0001,     # x1_current₊x1₊y(t) - Outward current activation gate
     -84.0       # V(t) - Resting potential [mV]
 ]
 
-p = [1.0,
-    50.0,
-    4.0,
-    50.0,
-    0.003,
-    0.09,
-    0.8
-]
+prob = ODEProblem(beeler_reuter_system, u0, (0.0, 400.0))
 
-# Time span for simulation
-tspan = (0.0, 500.0)  # 2000 ms
+using OrdinaryDiffEqTsit5, Plots
+sol = solve(prob, Tsit5())
+plot(sol, idxs=[8])
+# rlf = RushLarsenFunction(sys)
 
-prob = ODEProblem(rlf, u0, tspan, p)
 
-sol = solve(prob, RushLarsen(), dt=0.01)
+# p = [1.0,
+#     50.0,
+#     4.0,
+#     50.0,
+#     0.003,
+#     0.09,
+#     0.8
+# ]
+
+# # Time span for simulation
+# tspan = (0.0, 500.0)  # 2000 ms
+
+# prob = ODEProblem(rlf, u0, tspan, p)
+
+# sol = solve(prob, RushLarsen(), dt=0.01)
--- src/components.jl
+++ src/components.jl
@@ -5,7 +5,7 @@ is_gating_variable(var) = ModelingToolkit.Symbolics.hasmetadata(var, GatingVaria
 
 @component function AlphaBetaGate(; name, α_expr, β_expr)
     @variables begin
-        y(t), [gating = true]
+        y(t), [gating = true, bounds = (0.0, 1.0)]
         α(t)
         β(t)
     end
@@ -13,59 +13,78 @@ is_gating_variable(var) = ModelingToolkit.Symbolics.hasmetadata(var, GatingVaria
     eqs = [
         α ~ α_expr
         β ~ β_expr
-        D(y) ~ α * (1 - y) - β * y
+        D(y) ~ α * (1 - y) - β * y # = α - (α + β) y
     ]
 
     System(eqs, t, [y, α, β], [], name=name)
 end
 
-@component function SteadyStateTauGate(; name, y∞, τ)
-    @variables y(t)
+# TODO compatibility with Rush-Larsen
+#  we have α = 1/τ and β = 
+@component function TauGate(; name, y∞, τ)
+    @variables y(t), [bounds = (0.0, 1.0)]
 
     @parameters begin
         y∞ = y∞
         τ = τ
     end
 
-    eqs = [D(y) ~ (y∞ - y) / (τ)]
+    eqs = [
+        D(y) ~ (y∞ - y) / (τ)
+    ]
+
     System(eqs, t, [y], [y∞, τ], name=name)
 end
 
+# This is the lIₘit case when τ -> 0
+@component function RapidTauGate(; name, y∞_expr, τ)
+    @variables y(t), [bounds = (0.0, 1.0)]
 
-@component function IonChannelConductance(; name, g_max, gates, powers=nothing)
-    @variables g(t)
-    @parameters g_max = g_max
-
-    if isnothing(powers)
-        powers = ones(Int, length(gates))
+    @parameters begin
+        y∞ = y∞
     end
 
-    gate_product = prod(gate.y^exp for (gate, exp) in zip(gates, powers))
-
     eqs = [
-        g ~ g_max * gate_product
+        0 ~ y∞ - y,
+        y∞ ~ y∞_expr(t),
     ]
-
-    System(eqs, t, [g], [g_max], systems=[gates...], name=name)
+    System(eqs, t, [y, y∞], [y∞, τ], name=name)
 end
 
-"""
-NernstPotential(Xₑ, Xᵢ, zₓ, T)
+# @component function IonChannelConductance(; name, g_max, gates, powers=nothing)
+#     @variables g(t)
+#     @parameters g_max = g_max
 
-    Xₑ = extracellular concentration of X
-    Xᵢ = extracellular concentration of X
-    zₓ = charge of X
-    T  = absolute temperature
-"""
-@component function NernstPotential(; name, Xₑ, Xᵢ, zₓ, T)
-    @variables Eₓ(t)
-    # Faraday and Gas constants
-    @parameters F = 96.5 , R = 8.315
-    eqs = [
-        Eₓ ~ (zX * F) / (zₓ * R) * log(Xₑ / Xᵢ)
-    ]
+#     if isnothing(powers)
+#         powers = ones(Int, length(gates))
+#     end
 
-    System(eqs, t, [Eₓ], [F, R], systems=[gm], name=name)
+#     gate_product = prod(gate.y^exp for (gate, exp) in zip(gates, powers))
+
+#     eqs = [
+#         g ~ g_max * gate_product
+#     ]
+
+#     System(eqs, t, [g], [g_max], systems=[gates...], name=name)
+# end
+
+@mtkmodel NernstPotential begin
+    @constants begin
+        F = 96.5 # Faraday constant
+        R = 8.315 # Gas constant
+    end
+    @parameters begin
+        zₓ # Charge
+        T = 310.0  # Absolute temperature
+    end
+    @variables begin
+        Eₓ(t)
+        Xₑ(t) # concentration of X on side e (,,extracellular'')
+        Xᵢ(t) # concentration of X on side i (,,intracellular'')
+    end
+    @equations begin
+        Eₓ ~ (R * T) / (zₓ * F) * log(Xₑ / Xᵢ)
+    end
 end
 
 """
@@ -76,122 +95,181 @@ GoldmannHodgkinKatzPotential(Xₑs, Xᵢs, Pₓs, T)
     Pₓs = permeabilities of X
     T   = absolute temperature
 """
-@component function GoldmannHodgkinKatzPotential(; name, Xₑs, Xᵢs, Pₓs, T)
-    @variables Eₓ(t)
-    # Faraday and Gas constants
-    @parameters F = 96.5 , R = 8.315
-    eqs = [
-        Eₓ ~ (zX * F) / R * log(sum(Xₑs .* Pₓs) / sum(Xᵢs .* Pₓs))
-    ]
-
-    System(eqs, t, [Eₓ], [F, R], systems=[gm], name=name)
-end
-
-@component function OhmicCurrent(; name, Vm, gm, Ex)
-    @variables Im(t)
-    @parameters Ex = Ex
-    eqs = [
-        Im ~ gm.g * (Vm - Ex)
-    ]
-
-    System(eqs, t, [Vm, Im], [Ex], systems=[gm], name=name)
-end
-
-"""
-GoldmannHodgkinKatzCurrent(Xₑ, Xᵢ, Pₓ, zₓ, T)
-
-    Xₑs = extracellular concentrations of X
-    Xᵢs = extracellular concentrations of X
-    Pₓs = permeabilities of X
-    T   = absolute temperature
-"""
-@component function GoldmannHodgkinKatzCurrent(; name, φₘ, Xₑ, Xᵢ, Pₓ, zₓ, T)
-    @variables I(t)
-    # Faraday and Gas constants
-    @parameters F = 96.5 , R = 8.315
-    eqs = [
-        I ~ Pₓ * (zₓ^2 * F^2) / (R * T) * (Xₓ - Xₑ * exp(-zₓ) * F * φₘ / (R * T)) / (1 - exp(-zₓ) * F * φₘ / (R * T))
-    ]
-
-    System(eqs, t, [I], [F, R], systems=[gm], name=name)
-end
-
-@component function LeakCurrent(; name, Vm, gL, EL)
-    @variables Im(t)
-
+@mtkmodel GoldmannHodgkinKatzPotential begin
+    @structural_parameters begin
+        N # number of species
+    end
+    @constants begin
+        F = 96.5 # Faraday constant
+        R = 8.315 # Gas constant
+    end
     @parameters begin
-        gL = gL
-        EL = EL
+        zₓ # Charge
+        T = 310.0  # Absolute temperature
+    end
+    @variables begin
+        Eₓ(t)
+        Xₑs(t)[1:N]
+        Xᵢs(t)[1:N]
+        Pₓs(t)[1:N]
+    end
+    @equations begin
+        Eₓ ~ (zX * F) / R * log(sum(Xₑs .* Pₓs) / sum(Xᵢs .* Pₓs))
+    end
+end
+
+@mtkmodel OhmicCurrent begin
+    @parameters begin
+        g
     end
 
-    eqs = [
-        Im ~ gL * (Vm - EL)
-    ]
+    @variables begin
+        Iₘ(t)
+        φₘ(t)
+        E(t)
+    end
 
-    System(eqs, t, [Vm, Im], [EL, gL], name=name)
+    @equations begin
+        Iₘ ~ g * (φₘ - E)
+    end
 end
 
-@component function CompartmentSystem(; name, Vm, Cm, currents, stim)
-    @parameters Cm = Cm
-    Is = (current.Im for current in currents)
-
-    eqs = [D(Vm) ~ -1 / Cm * (sum(Is) - stim(t))]
-
-    System(eqs, t, [Vm], [Cm], systems=[currents...], name=name)
+@mtkmodel GoldmannHodgkinKatzCurrent begin
+    @constants begin
+        # Faraday and Gas constants
+        F = 96.5
+        R = 8.315
+    end
+    @parameters begin
+        T = 310 # absolute temperature
+        zₓ # charge
+        Pₓ # permability
+    end
+    @variables begin
+        Iₘ(t)
+        Xₑ(t)
+        Xᵢ(t)
+    end
+    @equations begin
+        Iₘ ~ Pₓ * (zₓ^2 * F^2) / (R * T) * (Xₓ - Xₑ * exp(-zₓ * F * φₘ) / (R * T)) / (1 - exp(-zₓ * F * φₘ) / (R * T))
+    end
 end
 
-# FIXME
-@component function SlowInwardCurrent(; name, Vm, Ca_i, gm)
-    @variables Im(t) E_s(t)
 
-    eqs = [
-        E_s ~ -82.3 - 13.0287 * log(Ca_i)
-        Im ~ gm.g * (Vm - E_s)
-    ]
+#########################
 
-    System(eqs, t, [Vm, Ca_i, Im, E_s], [], systems=[gm], name=name)
+
+@mtkmodel BeelerReuterIK1 begin
+    @variables begin
+        Iₘ(t)
+        ik2(t)
+        c1(t)
+        φₘ(t)
+    end
+    @equations begin
+        # This equation comes from Adrian (1969) Eqn 15.1.
+        ik2 ~ 4.0 * (exp(0.04 * (φₘ + 85.0)) - 1.0) / exp(0.08 * (φₘ + 53.0)) + exp(0.04 * (φₘ + 53.0))
+        # This is attributed to McAllister, which in turn also attributes this to Adrian (1969), but I cannot find the formula in the paper.
+        Iₘ ~ 0.35 * (ik2 + 0.2 * (φₘ + 23.0) / (1.0 - exp(-0.04 * (φₘ + 23.0))))
+    end
 end
 
-# FIXME
-@component function TimeIndependentK(; name, Vm)
-    @variables Im(t)
-
-    eqs = [
-        Im ~ 0.35 * (4.0 * (exp(0.04 * (Vm + 85.0)) - 1.0) /
-                     (exp(0.08 * (Vm + 53.0)) + exp(0.04 * (Vm + 53.0))) +
-                     0.2 * (Vm + 23.0) / (1.0 - exp(-0.04 * (Vm + 23.0))))
-    ]
-
-    System(eqs, t, [Vm, Im], [], name=name)
+# TODO in their paper they do not state if they got this function from somewhere else. Double check.
+function BeelerReuterRate(φₘ, C₁, C₂, C₃, C₄, C₅, C₆, C₇)
+    (C₁ * exp(C₂ * (φₘ - C₃)) + C₄ * (φₘ - C₅)) / (exp(C₆ * (φₘ - C₃)) + C₇)
 end
 
-# FIXME
-@component function TimeActivatedOutward(; name, Vm, gate, g_x1)
-    @variables Im(t)
-    @parameters g_x1 = g_x1
-
-    eqs = [
-        Im ~ g_x1 * gate.y * (exp(0.04 * (Vm + 77)) - 1) / exp(0.04 * (Vm + 35))
-    ]
-
-    System(eqs, t, [Vm, Im], [g_x1], systems=[gate], name=name)
+@mtkmodel BeelerReuterIx1 begin
+    @variables begin
+        Iₘ(t)
+        φₘ(t)
+    end
+    @parameters begin
+        g_x1 = 0.8
+    end
+    @components begin
+        gate =  AlphaBetaGate(
+            # TODO pull parameters from parameter array instead of hardcoding
+            α_expr=BeelerReuterRate(φₘ, 0.0005, 0.083, 50.0, 0, 0, 0.057, 1.0),
+            β_expr=BeelerReuterRate(φₘ, 0.0013, -0.06, 20.0, 0, 0, -0.04, 1.0),
+        )
+    end
+    @equations begin
+        Iₘ ~ g_x1 * gate.y * (exp(0.04 * (φₘ + 77)) - 1) / exp(0.04 * (φₘ + 35))
+    end
 end
 
-# FIXME
-@component function CalciumDynamics(; name, Ca_i, I_s)
-    eqs = [
-        D(Ca_i) ~ -10.0^-7 * I_s + 0.07 * (10.0^-7 - Ca_i)
-    ]
-
-    System(eqs, t, [Ca_i, I_s], [], name=name)
+@mtkmodel BeelerReuterICa begin
+    @parameters begin
+        g_max = 0.09
+    end
+    @variables begin
+        φₘ(t)
+        ECa(t)
+        Iₘ(t)
+    end
+    @components begin
+        d = AlphaBetaGate(
+            # TODO pull parameters from parameter array instead of hardcoding
+            α_expr=BeelerReuterRate(φₘ, 0.095, -0.01, -5.0, 0, 0, -0.072, 1.0),
+            β_expr=BeelerReuterRate(φₘ, 0.07, -0.017, 44.0, 0, 0, 0.05, 1.0),
+        )
+        f = AlphaBetaGate(
+            # TODO pull parameters from parameter array instead of hardcoding
+            α_expr=BeelerReuterRate(φₘ, 0.012, -0.008, 28.0, 0, 0, 0.15, 1.0),
+            β_expr=BeelerReuterRate(φₘ, 0.0065, -0.02, 30.0, 0, 0, -0.2, 1.0),
+        )
+        i = OhmicCurrent(g=g_max)
+    end
+    @equations begin
+        i.φₘ ~ φₘ
+        i.E ~ ECa
+        Iₘ ~ g_max * f.y * d.y * i.Iₘ
+    end
 end
 
-@component function StimulationSystem(; name, stim_func)
-    @variables I_stim(t)
+@mtkmodel BeelerReuterINa begin
+    @parameters begin
+        g_max = 4.0
+        ENa = 50.0
+    end
+    @variables begin
+        φₘ(t)
+        Iₘ(t)
+    end
+    @components begin
+        # TODO use BeelerReuterRate
+        m = AlphaBetaGate(
+            α_expr=(φₘ + 47.0) / (1.0 - exp(-0.1 * (φₘ + 47.0))),
+            β_expr=40.0 * exp(-0.056 * (φₘ + 72.0))
+        )
+        h = AlphaBetaGate(
+            α_expr=0.126 * exp(-0.25 * (φₘ + 77.0)),
+            β_expr=1.7 / (1.0 + exp(-0.082 * (φₘ + 22.5)))
+        )
+        j = AlphaBetaGate(
+            α_expr=0.055 * exp(-0.25 * (φₘ + 78)) / (1 + exp(-0.2 * (φₘ + 78))),
+            β_expr=0.3 / (1 + exp(-0.1 * (φₘ + 32.0)))
+        )
+        i = OhmicCurrent(g=g_max)
+    end
+    @equations begin
+        i.φₘ ~ φₘ
+        i.E ~ ENa
+        Iₘ ~ g_max * m.y^3 * h.y * j.y * i.Iₘ
+    end
+end
 
-    eqs = [
-        I_stim ~ stim_func(t)
-    ]
-
-    System(eqs, t, [I_stim], [], name=name)
+@mtkmodel BeelerReuterCalciumDynamics begin
+    @parameters begin
+        Caₒ   = 0.0021, [description = "Extracellular calcium concentration"]
+        T     = 310.0, [description = "Absolute temperature"]
+    end
+    @variables begin
+        Caᵢ(t)
+        iCa(t)
+    end
+    @equations begin
+        D(Caᵢ) ~ -10.0^-7 * iCa + 0.07 * (10.0^-7 - Caᵢ)
+    end
 end
